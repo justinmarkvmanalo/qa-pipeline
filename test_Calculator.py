108f20a4c6be483022e5d05fb7b61df2e95df949
Justin Mark <justinmarkvmanalo07@gmail.com>
2025-04-10 21:37:21 +0800

    update

--- test_Calculator.py
+++ test_Calculator.py
@@ -8,36 +8,36 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(3, 7), 10)
+        self.assertEqual(self.calc.add(2, 9), 11)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(10, 4), 6)
+        self.assertEqual(self.calc.subtract(15, 7), 8)
 
     def test_multiply(self):
-        self.assertEqual(self.calc.multiply(5, 5), 25)
+        self.assertEqual(self.calc.multiply(6, 4), 24)
 
     def test_divide(self):
-        self.assertEqual(self.calc.divide(8, 2), 4)
+        self.assertEqual(self.calc.divide(12, 3), 4)
 
     def test_divide_by_zero(self):
         with self.assertRaises(ValueError):
-            self.calc.divide(5, 0)
+            self.calc.divide(7, 0)
 
     def test_power(self):
-        self.assertEqual(self.calc.power(2, 3), 8)
+        self.assertEqual(self.calc.power(3, 4), 81)
 
     def test_mod(self):
-        self.assertEqual(self.calc.mod(10, 3), 1)
+        self.assertEqual(self.calc.mod(20, 6), 2)
 
     def test_mod_by_zero(self):
         with self.assertRaises(ValueError):
-            self.calc.mod(10, 0)
+            self.calc.mod(15, 0)
 
     def test_sqrt(self):
-        self.assertEqual(self.calc.sqrt(16), 4)
-        self.assertEqual(self.calc.sqrt(0), 0)
+        self.assertEqual(self.calc.sqrt(25), 5)
+        self.assertEqual(self.calc.sqrt(9), 3)
         with self.assertRaises(ValueError):
-            self.calc.sqrt(-4)
+            self.calc.sqrt(-16)
 
 
 if __name__ == '__main__':
